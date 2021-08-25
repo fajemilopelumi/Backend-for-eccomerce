30b70a21e754f333fd0dd4966d558fa2b0234881
unknown <pfajemilo@gmail.com>
2021-08-25 15:49:31 -0700

    implementing the admin section

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from 'path';
 import dotenv from 'dotenv';
-import productRouter from "./router/productRouter.js";
-import userRouter from "./router/userRouter.js";
-import orderRouter from "./router/orderRouter.js";
+import productRouter from "./backend/router/productRouter.js";
+import userRouter from "./backend/router/userRouter.js";
+import orderRouter from "./backend/router/orderRouter.js";
+import uploadRouter from './backend/router/uploadRouter.js';
 
 dotenv.config()
 
@@ -16,17 +18,22 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useCreateIndex: true,
 }) 
 
-
+app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter)
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouter)
 app.get('/api/config/paypal', (req,res)=>{
   res.send(process.env.PAYPAL_CLIENT_ID ||'sb')
-})
-app.get("/", (req, res) => {
-  res.send("Server is ready"); 
 });
-
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 app.use((err,req,res,next)=>{
   res.status(500).send({message: err.message})
 })

c44da355e159952ba677376977a52aff8fb77aef
unknown <pfajemilo@gmail.com>
2021-08-27 06:14:33 -0700

    import path

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ app.use((err,req,res,next)=>{
 })
 
 if(process.env.NODE_ENV === 'production'){
-  const path = require('path');
   app.get('/*', (req,res)=>{
     res.sendFile(path.resolve(__dirname,'./my-eccomerce','build','index.html'))
   })

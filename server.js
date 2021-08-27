17e17025878e9b96b7d116f28e5fd5c49f44fdfe
unknown <pfajemilo@gmail.com>
2021-08-27 06:02:51 -0700

    -

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import path from 'path';
 import dotenv from 'dotenv';
-import productRouter from "./router/productRouter.js";
-import userRouter from "./router/userRouter.js";
-import orderRouter from "./router/orderRouter.js";
-import uploadRouter from './router/uploadRouter.js';
+import productRouter from "./backend/router/productRouter.js";
+import userRouter from "./backend/router/userRouter.js";
+import orderRouter from "./backend/router/orderRouter.js";
+import uploadRouter from './backend/router/uploadRouter.js';
 
 dotenv.config()
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-app.use(express.static('my-eccomerce/build'))
+
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true, 
@@ -20,8 +20,10 @@ mongoose.connect(process.env.MONGODB_URL, {
   useCreateIndex: true,
 }) 
 
+app.use(express.static('my-eccomerce/build'))
+
 app.use('/api/uploads', uploadRouter);
-app.use('/api/users', userRouter)
+app.use('/api/users', userRouter) 
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouter)
 app.get('/api/config/paypal', (req,res)=>{
@@ -41,12 +43,12 @@ app.use((err,req,res,next)=>{
   res.status(500).send({message: err.message})
 })
 
-// if(process.env.NODE_ENV === 'production'){
-//   const path = require('path');
-//   app.get('/*', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname,'../my-eccomerce','build','index.html'))
-//   })
-// }
+if(process.env.NODE_ENV === 'production'){
+  const path = require('path');
+  app.get('/*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./my-eccomerce','build','index.html'))
+  })
+}
 const port = process.env.PORT || 300; 
 
 app.listen(port, () => {

b40abb796b857ad2805cbae3710d067936ca3b9e
unknown <pfajemilo@gmail.com>
2021-08-31 02:05:19 -0700

    initil

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: true}))
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
-  useCreateIndex: true,
+  useCreateIndex: true
 })  
 
 
